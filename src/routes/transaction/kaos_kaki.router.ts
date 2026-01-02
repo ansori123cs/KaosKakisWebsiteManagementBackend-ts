45abcc2e1f6af337d05c06788983237ba04eff9e
ansori123cs <ahmadansori985@gmail.com>
2026-01-03 06:01:58 +0700

    fix minor routes bug

--- src/routes/transaction/kaos_kaki.router.ts
+++ src/routes/transaction/kaos_kaki.router.ts
@@ -4,16 +4,17 @@ import {
   newkaosKakiData,
   FormDataKaosKaki,
   updatekaosKakiData,
+  deletekaosKakiData,
 } from "../../controllers/transaction/kaos_kaki.transaction.controller.ts";
 import { Router } from "express";
 
 const kaosKakiRouter = Router();
 
 kaosKakiRouter.get("/", getKaosKakiData);
-kaosKakiRouter.get("/form", FormDataKaosKaki);
+kaosKakiRouter.get("/form/:id", FormDataKaosKaki);
 kaosKakiRouter.get("/:id", getKaosKakiDetails);
 kaosKakiRouter.post("/create", newkaosKakiData);
-kaosKakiRouter.post("/update/:id", updatekaosKakiData);
-kaosKakiRouter.post("/delete/:id", newkaosKakiData);
+kaosKakiRouter.put("/update/:id", updatekaosKakiData);
+kaosKakiRouter.delete("/delete/:id", deletekaosKakiData);
 
 export default kaosKakiRouter;
