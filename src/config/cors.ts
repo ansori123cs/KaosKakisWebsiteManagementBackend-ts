f6e41e234ae44f65ac5b53ee1096898333ba3da3
ansori123cs <ahmadansori985@gmail.com>
2026-03-06 15:26:18 +0700

    add whitelist  cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const whitelist = isProd
   ? ["https://website-anda.com"]
-  : ["http://localhost:5173", "http://127.0.0.1:5173", "http://127.0.0.1:5500"];
+  : ["http://localhost:5173", "http://127.0.0.1:5173", "http://127.0.0.1:5500","http://127.0.0.1:3000"];
 
 export const corsOptions: cors.CorsOptions = {
   origin: function (origin, callback) {
